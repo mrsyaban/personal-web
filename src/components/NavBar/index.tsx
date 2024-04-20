38028e0c279edee1429af7fe9dfbd181d1c9d415
mrsyaban <mrsyaban99@gmail.com>
2024-04-20 22:08:00 +0700

    feat: update cv

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,6 +1,5 @@
 import { Link } from 'react-router-dom';
 import logo from './../../assets/logo.svg';
-import {FaLinkedin, FaInstagram, FaGithub} from 'react-icons/fa';
 
 const NavBar: React.FC = () => {
   const scrollToTop = () => { 
@@ -41,32 +40,6 @@ const NavBar: React.FC = () => {
       </div>
 
 
-      <div className="flex bg-transparent pt-2 space-x-3 h-auto text-navbarBlue">
-        <a
-          href="https://www.linkedin.com/in/mrsyaban/"
-          rel="noreferrer"
-          target="_blank"
-          className="hover:text-blue"
-        >
-          <FaLinkedin size={30} />
-        </a>
-        <a
-          href="https://www.instagram.com/mrsybn_/"
-          rel="noreferrer"
-          target="_blank"
-          className="hover:text-blue"
-        >
-          <FaInstagram size={30} />
-        </a>
-        <a
-          href="https://github.com/mrsyaban"
-          rel="noreferrer"
-          target="_blank"
-          className="hover:text-blue"
-        >
-          <FaGithub size={30} />
-        </a>
-      </div>
     </nav>
   );
 };
--- src/components/homepage/about-me/index.tsx
+++ src/components/homepage/about-me/index.tsx
@@ -1,5 +1,6 @@
 import profpic from './../../../assets/propil.svg';
 import { FaFileAlt } from "react-icons/fa";
+import {FaLinkedin, FaInstagram, FaGithub} from 'react-icons/fa';
 
 const AboutMe = () => {
   return (
@@ -17,20 +18,49 @@ const AboutMe = () => {
           industries and bring new solutions to market through creative
           problem-solving and teamwork.
         </p>
-        <a 
-          className="flex  justify-start " 
-          href="https://drive.google.com/file/d/1iT-xcs3OR3-uhCq1FCxIGI-qRtxp0qQ4/view?usp=drive_link" 
-          rel="noreferrer" 
-          target="_blank"
-        >
-          <button className="flex hover:pl-5 bottom-0 items-center gap-14 border-b border-solid border-blue text-[16px] pb-2 md:gap-32 ">
-            <p>
-              G E T  &nbsp;  C V  &nbsp;
-              <span className='text-slate-400'>(PDF)</span>
-            </p>
-            <FaFileAlt size={16} />
-          </button>
-        </a>
+        <div className='flex flex-row gap-12'>
+          <a 
+            className="flex  justify-start" 
+            href="./CV_Muhammad-Rizky-Sya'ban.pdf" 
+            rel="noopener noreferrer" 
+            target="_blank"
+          >
+            <button className="flex hover:pl-5 bottom-0 items-center gap-14 border-b border-solid border-blue text-[16px] pb-2 md:gap-32 ">
+              <p>
+                G E T  &nbsp;  C V  &nbsp;
+                <span className='text-slate-400'>(PDF)</span>
+              </p>
+              <FaFileAlt size={16} />
+            </button>
+          </a>
+        
+          <div className="flex bg-transparent pt-2 space-x-3 h-auto text-navbarBlue">
+            <a
+              href="https://www.linkedin.com/in/mrsyaban/"
+              rel="noreferrer"
+              target="_blank"
+              className="hover:text-blue"
+            >
+              <FaLinkedin size={30} />
+            </a>
+            <a
+              href="https://www.instagram.com/mrsybn_/"
+              rel="noreferrer"
+              target="_blank"
+              className="hover:text-blue"
+            >
+              <FaInstagram size={30} />
+            </a>
+            <a
+              href="https://github.com/mrsyaban"
+              rel="noreferrer"
+              target="_blank"
+              className="hover:text-blue"
+            >
+              <FaGithub size={30} />
+            </a>
+          </div>
+        </div>
       </div>
       <div className='flex lg:flex-col items-center h-fit justify-center'>
         <img 
